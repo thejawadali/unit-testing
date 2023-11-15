d805d459ed6ade9d0b682c32e03f9c87fc160128
Jawad Ali <jawad.ali@repairdesk.co>
2023-11-15 14:54:08 +0500

    made greeting component props, ref, and emit

---
+++ src/components/TheGreetings.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>
+      {{ ha ? greeting : 'good bye' }}
+    </h1>
+    <p @click="emit('change')">{{formattedName}}</p>
+  </div>
+</template>
+
+<script setup>
+import { ref, computed, defineProps, defineEmits } from 'vue';
+
+const firstName = ref('John');
+const lastName = ref('Doe');
+
+const formattedName = computed(() => {
+  return `${firstName.value} ${lastName.value}`;
+});
+
+const props = defineProps({
+  greeting: String,
+  ha: Boolean
+})
+
+const emit = defineEmits(['change'])
+
+</script>
